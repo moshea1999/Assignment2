2ec8b464a1366030cecb96cf87808b5c9044825e
moshea1999 <62572311+moshea1999@users.noreply.github.com>
2020-03-25 11:18:26 +0000

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -16,19 +16,88 @@ namespace Assignment2
             String[] GitRepositories_3a = File.ReadAllLines(@"C:\Users\Owner\Documents\GitRepositories_3a.txt");
             String[] GitRepositories_3b = File.ReadAllLines(@"C:\Users\Owner\Documents\GitRepositories_3b.txt");
 
-            Console.WriteLine("Please choose 1: ");
-            Console.WriteLine("1a");
-            Console.WriteLine("1b");
-            Console.WriteLine("2a");
-            Console.WriteLine("2b");
-            Console.WriteLine("3a");
-            Console.WriteLine("3b");
+            {
+                Console.WriteLine("Please choose 1: ");
+                Console.WriteLine("1a");
+                Console.WriteLine("1b");
+                Console.WriteLine("2a");
+                Console.WriteLine("2b");
+                Console.WriteLine("3a");
+                Console.WriteLine("3b");
 
-            string input = "";
-            String[] input1 = { "" };
-            String[] input2 = { "" };
+                string input = "";
+                String[] input1 = { "" };
+                String[] input2 = { "" };
 
-            input = Console.ReadLine().ToLower();
+                input = Console.ReadLine().ToLower();
+                switch (input)
+
+                {
+                    case "1a":
+                        input1 = GitRepositories_1a;
+                        break;
+
+                    case "1b":
+                        input1 = GitRepositories_1b;
+                        break;
+
+                    case "2a":
+                        input1 = GitRepositories_2a;
+                        break;
+
+                    case "2b":
+                        input1 = GitRepositories_2b;
+                        break;
+
+                    case "3a":
+                        input1 = GitRepositories_3a;
+                        break;
+
+                    case "3b":
+                        input1 = GitRepositories_3b;
+                        break;
+
+
+                }
+
+                Console.WriteLine("Please choose 1: ");
+                Console.WriteLine("1a");
+                Console.WriteLine("1b");
+                Console.WriteLine("2a");
+                Console.WriteLine("2b");
+                Console.WriteLine("3a");
+                Console.WriteLine("3b");
+
+                input = Console.ReadLine().ToLower();
+                switch (input)
+
+                {
+                    case "1a":
+                        input1 = GitRepositories_1a;
+                        break;
+
+                    case "1b":
+                        input1 = GitRepositories_1b;
+                        break;
+
+                    case "2a":
+                        input1 = GitRepositories_2a;
+                        break;
+
+                    case "2b":
+                        input1 = GitRepositories_2b;
+                        break;
+
+                    case "3a":
+                        input1 = GitRepositories_3a;
+                        break;
+
+                    case "3b":
+                        input1 = GitRepositories_3b;
+                        break;
+                }
+
+            }
         }
     }
 }
